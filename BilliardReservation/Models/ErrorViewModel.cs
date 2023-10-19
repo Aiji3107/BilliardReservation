53f3edcdb6697eb895cefff0c4c651d065ca1ff5
aghikalam <aghikalam2@gmail.com>
2023-10-19 21:38:08 +0700

    Add project files.

---
+++ BilliardReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BilliardReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
